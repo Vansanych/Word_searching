3d8d5541af23e996ba56d201241dd0ef66b3fecd
Иван <e741@mail.ru>
2022-08-28 11:00:58 +0300

    comments added, vars cut

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import docx
 
 doc = docx.Document(r'C:\Users\Иван\Desktop\Иван\python\pythonProject\python-docx\01-ТК 001-ППР001-08-2020 Геодез.docx')
 
-print(len(doc.tables))
-print(len(doc.tables[1].rows))
-print(len(doc.tables[1].rows[0].cells))
-
 target_cell = doc.tables[1].rows[2].cells[0]
 target_text = target_cell.paragraphs[0].runs[1].text
 
@@ -13,12 +9,20 @@ target_cell.paragraphs[0].runs[1].text = str('Какой-то текст 4')
 print(target_cell.paragraphs[0].runs[1].text)
 
 
-doc.save(r'C:\Users\Иван\Desktop\Иван\python\pythonProject\python-docx\01-ТК 001-ППР001-08-2020 Геодез1.docx')
+# doc.save(r'C:\Users\Иван\Desktop\Иван\python\pythonProject\python-docx\01-ТК 001-ППР001-08-2020 Геодез1.docx')
+
+tables = doc.tables
+rows1 = tables[1].rows
+cells0 = rows1[0].cells
+
+print(len(tables))
+print(len(rows1))
+print(len(cells0))
 
 tables_text = []
-for row in range(len(doc.tables[1].rows)):
-    for cell in range(len(doc.tables[1].rows[row].cells)):
-        tables_text.append(doc.tables[1].rows[row].cells[cell].text)
+for row in range(len(rows1)):  # создает список из текста в ячейках
+    for cell in range(len(rows1[row].cells)):
+        tables_text.append(rows1[row].cells[cell].text)
 
 
 tables_word = []  # создание списка из слов в ячейках
@@ -27,6 +31,7 @@ for row in range(len(doc.tables[1].rows)):
         for word in range(len(doc.tables[1].rows[row].cells[cell].text.split())):
             if doc.tables[1].rows[row].cells[cell].text.split()[word] not in tables_word:
                 tables_word.append(doc.tables[1].rows[row].cells[cell].text.split()[word])
+
 
 print(tables_text)
 print(tables_word)
